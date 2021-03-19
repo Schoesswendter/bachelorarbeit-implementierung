a1a532efaf53ae0ba81ca34ae586273537a52f4e
nillmichaela <michaela.schoesswendter@nill.at>
2021-03-19 09:42:42 +0100

    add prefill

---
+++ public/javascripts/prefill.js
@@ -0,0 +1,19 @@
+var questionlocation = location.href.indexOf('?');
+function FillForm(){
+  if(questionlocation < 0) { return; }
+
+  var q = location.href.substr(questionlocation + 1);
+
+  var list = q.split('&');
+  for(var i = 0; i < list.length; i++) {
+
+    var param = list[i].split('=')[0]
+    var value = list[i].split('=')[1]
+
+    document.getElementsByName(param)[0].value = value;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  FillForm();
+})
