f0d12ad761eeada0d78d5792be02e13917343f58
nillmichaela <michaela.schoesswendter@nill.at>
2021-03-29 16:31:48 +0200

    add corona charts

--- public/javascripts/corona_charts.js
+++ public/javascripts/corona_charts.js
@@ -1,12 +1,35 @@
+var country = ""
+
+var select_countries = getElementByClass("country")
+
+select_countries.forEach(element => {
+  element.addEventListener
+});
+
+var corona_data = {}
+
+await fetch(`https://coronavirus-19-api.herokuapp.com/countries/${country}`)
+.then((resp) => resp.json())
+.then(function(data) {
+  console.log("data", data);
+  corona_data = data
+})
+.catch(function(error) {
+  console.log(error);
+});
+
+console.log("All data", corona_data)
+console.log("cases", corona_data.cases)
+
 var ctx = document.getElementById('myChart');
 
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Fälle', 'Aktive Fälle', 'Tode durch Corona', 'Geheilte Fälle'],
       datasets: [{
           label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          data: [corona_data["cases"], corona_data["active"], corona_data["deaths"], corona_data["recovered"]],
           backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
