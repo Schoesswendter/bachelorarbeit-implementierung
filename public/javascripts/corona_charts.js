0e63f9cb84fa7912ca81d5128e96d94f4dd2215f
nillmichaela <michaela.schoesswendter@nill.at>
2021-03-30 15:44:56 +0200

    add pie chart, add selection of country

--- public/javascripts/corona_charts.js
+++ public/javascripts/corona_charts.js
@@ -1,4 +1,4 @@
-var country = ""
+var country = "Austria"
 
 var select_countries = document.getElementsByClassName("country")
 
@@ -10,14 +10,14 @@ console.log(select_countries)
 var corona_data = {}
 
 await fetch(`https://coronavirus-19-api.herokuapp.com/countries/${country}`)
-.then((resp) => resp.json())
-.then(function(data) {
-  console.log("data", data);
-  corona_data = data
-})
-.catch(function(error) {
-  console.log(error);
-});
+  .then((resp) => resp.json())
+  .then(function (data) {
+    console.log("data", data);
+    corona_data = data
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 console.log("All data", corona_data)
 console.log("cases", corona_data["cases"])
@@ -27,36 +27,105 @@ var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-      labels: ['Fälle', 'Aktive Fälle', 'Tode durch Corona', 'Geheilte Fälle'],
-      datasets: [{
-          label: '# of Votes',
-          data: [corona_data["cases"], corona_data["active"], corona_data["deaths"], corona_data["recovered"]],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
+    labels: ['Fälle', 'Aktive Fälle', 'Tode durch Corona', 'Geheilte Fälle'],
+    datasets: [{
+      label: 'Personen',
+      data: [corona_data["cases"], corona_data["active"], corona_data["deaths"], corona_data["recovered"]],
+      backgroundColor: [
+        'rgba(54, 162, 235, 0.9)',
+        'rgba(255, 206, 86, 0.9)',
+        'rgba(255, 99, 132, 0.9)',
+        'rgba(75, 192, 192, 0.9)'
+      ]
+    }]
   },
   options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          max: 4000000
+        }
+      }]
+    }
   }
 });
+
+document.getElementById("country--list").addEventListener("change", function (e) {
+  let selected = this.options[this.selectedIndex].value
+
+  console.log(selected)
+
+  updateData(myChart, selected)
+})
+
+var ctx_pie = document.getElementById('myPieChart');
+
+var myPieChart = new Chart(ctx_pie, {
+  type: 'pie',
+  data: {
+    labels: ['Fälle', 'Aktive Fälle', 'Tode durch Corona', 'Geheilte Fälle'],
+    datasets: [{
+      label: 'Personen',
+      data: [corona_data["active"], corona_data["deaths"], corona_data["recovered"]],
+      backgroundColor: [
+        'rgba(255, 206, 86, 0.9)',
+        'rgba(255, 99, 132, 0.9)',
+        'rgba(75, 192, 192, 0.9)'
+      ]
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          max: 4000000
+        }
+      }]
+    }
+  }
+});
+
+document.getElementById("country--list--pie").addEventListener("change", function (e) {
+  let selected = this.options[this.selectedIndex].value
+
+  updateData(myPieChart, selected, 'pie')
+})
+
+
+async function updateData(chart, selected, chart_type = 'bar') {
+  let new_data = {}
+
+  console.log(selected)
+
+  await fetch(`https://coronavirus-19-api.herokuapp.com/countries/${selected}`)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log("data", data);
+      new_data = data
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  
+  if (chart_type == 'bar') {
+    chart.data.datasets[0].data[0] = new_data["cases"]
+    chart.data.datasets[0].data[1] = new_data["active"]
+    chart.data.datasets[0].data[2] = new_data["deaths"]
+    chart.data.datasets[0].data[3] = new_data["recovered"]
+  }
+  else {
+    chart.data.datasets[0].data[0] = new_data["active"]
+    chart.data.datasets[0].data[1] = new_data["deaths"]
+    chart.data.datasets[0].data[2] = new_data["recovered"]
+  }
+  
+  chart.update();
+}
