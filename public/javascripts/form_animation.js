f631c94964e094007767f852aa9370ec667e8cfb
Schoesswendter <m.schoesswendter@gmx.at>
2021-04-01 21:42:47 +0200

    add style and js

---
+++ public/javascripts/form_animation.js
@@ -0,0 +1,39 @@
+function next(target) {
+  var input = target.previousElementSibling;
+  
+  // Check if input is empty
+  if (input.value === '') {
+    body.classList.add('error');
+  } else {
+    body.classList.remove('error');
+    
+    var enable = document.querySelector('form fieldset.enable'),
+        nextEnable = enable.nextElementSibling;
+    enable.classList.remove('enable');
+    enable.classList.add('disable');
+    nextEnable.classList.add('enable');
+    
+    // Switch active class on left list
+    var active = document.querySelector('ul.items li.active'),
+        nextActive = active.nextElementSibling;
+    active.classList.remove('active');
+    nextActive.classList.add('active');
+  }
+}
+
+function keyDown(event) {
+  var key = event.keyCode,
+      target = document.querySelector('fieldset.enable .button');
+  // keycode 13 = enter keycode 9 = tabulator
+  if (key == 13 || key == 9) next(target);
+}
+
+var body = document.querySelector('body'),
+    form = document.querySelector('form'),
+    count = form.querySelectorAll('fieldset').length;
+
+document.body.onmouseup = function (event) {
+    var target = event.target || event.toElement;
+    if (target.classList.contains("button")) next(target);
+};
+document.addEventListener("keydown", keyDown, false);
