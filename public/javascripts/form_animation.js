73b664415e1a965a8c89b1e29a8af443607b77d2
Schoesswendter <m.schoesswendter@gmx.at>
2021-04-02 22:46:21 +0200

    add next and back buttons and functionality

--- public/javascripts/form_animation.js
+++ public/javascripts/form_animation.js
@@ -1,21 +1,21 @@
 function next(target) {
-  var input = target.previousElementSibling;
-  
+  var input = target.closest('.navigation').previousElementSibling;
+
   // Check if input is empty
-  if (input.value === '') {
-    body.classList.add('error');
+  if (!input.checkValidity() || (input.nodeName == "select" && input.value == "")) {
+    document.querySelector('body').classList.add('error');
   } else {
-    body.classList.remove('error');
-    
-    var enable = document.querySelector('form label.enable'),
-        nextEnable = enable.nextElementSibling;
-    enable.classList.remove('enable');
+    document.querySelector('body').classList.remove('error');
+
+    var enable = document.querySelector('fieldset.enabled'),
+      nextEnable = enable.nextElementSibling;
+    enable.classList.remove('enabled');
     enable.classList.add('disable');
-    nextEnable.classList.add('enable');
-    
+    nextEnable.classList.add('enabled');
+
     // Switch active class on left list
     var active = document.querySelector('ul.items li.active'),
-        nextActive = active.nextElementSibling;
+      nextActive = active.nextElementSibling;
     active.classList.remove('active');
     nextActive.classList.add('active');
   }
@@ -23,17 +23,19 @@ function next(target) {
 
 function keyDown(event) {
   var key = event.keyCode,
-      target = document.querySelector('fieldset.enable .button');
+    target = document.querySelector('fieldset.enable .button');
   // keycode 13 = enter keycode 9 = tabulator
   if (key == 13 || key == 9) next(target);
 }
 
-var body = document.querySelector('body'),
+window.onload = function () {
+  var body = document.querySelector('body'),
     form = document.querySelector('form'),
-    count = form.querySelectorAll('label').length;
+  count = form.querySelectorAll('fieldset').length;
 
-document.body.onmouseup = function (event) {
+  document.body.onmouseup = function (event) {
     var target = event.target || event.toElement;
-    if (target.classList.contains("button")) next(target);
-};
-document.addEventListener("keydown", keyDown, false);
+    if (target.classList.contains("next")) next(target);
+  };
+  document.addEventListener("keydown", keyDown, false);
+}
